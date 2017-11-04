912365c7f8f2a7a72ca0a5928efe01da00f42549
Rhys Keepence <rhys@rhyskeepence.com>
2017-11-04 21:05:15 +0000

    parse all the commands

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Parser where
+module Parser (parse) where
 
 import Commands
 import qualified Text.Parsec as P
@@ -16,7 +16,37 @@ parse input =
   in mapLeft (ParseError . pack . show) parseResult
 
 parseCommand :: Parser UserCommand
-parseCommand = do
+parseCommand = P.choice [parseCreateCanvas, parseDrawLine,parseDrawRectangle, parseQuit]
+
+parseCreateCanvas :: Parser UserCommand
+parseCreateCanvas = do
+  void $ string "C"
+  width <- space >> decimal
+  height <- space >> decimal
+  return $ CanvasCommand $ CreateCanvas width height
+
+parseDrawLine :: Parser UserCommand
+parseDrawLine = do
+  void $ string "L"
+  x1 <- space >> decimal
+  y1 <- space >> decimal
+  x2 <- space >> decimal
+  y2 <- space >> decimal
+  return $ CanvasCommand $ DrawLine (x1, y1) (x2, y2)
+
+parseDrawRectangle :: Parser UserCommand
+parseDrawRectangle = do
+  void $ string "R"
+  x1 <- space >> decimal
+  y1 <- space >> decimal
+  x2 <- space >> decimal
+  y2 <- space >> decimal
+  return $ CanvasCommand $ DrawRectangle (x1, y1) (x2, y2)
+
+parseQuit :: Parser UserCommand
+parseQuit = do
   void $ string "Q"
   return Quit
+
+decimal = read <$> P.many1 digit
 
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -9,7 +9,20 @@ import Test.Hspec
 
 spec :: Spec
 spec = describe "Parser" $ do
+  it "can parse C to create canvas" $
+    parse "C 20 5" `shouldBe` Right (CanvasCommand(CreateCanvas 20 5))
+  it "can parse L to draw a line" $
+    parse "L 5 5 15 15" `shouldBe` Right (CanvasCommand(DrawLine (5, 5) (15, 15)))
+  it "can parse R to draw a rectangle" $
+    parse "R 1 1 20 20" `shouldBe` Right (CanvasCommand(DrawRectangle (1, 1) (20, 20)))
   it "can parse Q for quit" $
     parse "Q" `shouldBe` Right Quit
-  it "errors on unknown command" $
+
+  it "returns error on incorrect C arguments" $
+    parse "C FOO" `shouldSatisfy` isLeft
+  it "returns error on incorrect L arguments" $
+    parse "L FOO" `shouldSatisfy` isLeft
+  it "returns error on incorrect R arguments" $
+    parse "R FOO" `shouldSatisfy` isLeft
+  it "returns error on unknown command" $
     parse "FOO" `shouldSatisfy` isLeft
