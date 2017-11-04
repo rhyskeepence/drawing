dd61898032b1b76f262efe829108541d124d6c12
Rhys Keepence <rhys@rhyskeepence.com>
2017-11-04 21:09:37 +0000

    run continuously

--- app/Drawing.hs
+++ app/Drawing.hs
@@ -18,21 +18,17 @@ instance UserInput IO where
   write text = Data.Text.IO.putStr text >> hFlush stdout
   writeLine = Data.Text.IO.putStrLn
 
-errorMessage :: DrawingError -> Text
-errorMessage e = case e of
-  ParseError msg -> msg
-
 run :: UserInput m => CanvasState -> m ()
 run state = do
   write "Enter Command: "
   input <- readLine
   let command = parse input
   case command of
-    Right Quit ->
-      return ()
+    Left (ParseError msg) -> do
+      writeLine msg
+      run state
 
-    Left err -> do
-      writeLine $ errorMessage err
+    Right Quit ->
       return ()
 
     Right (CanvasCommand _) ->
